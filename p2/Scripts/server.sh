db10fee70e5dad7b3ad706127080e1ff5fb61abc
jean-Michel <jean.michel.cervoni@gmail.com>
2023-12-20 15:32:57 +0100

    UPDATE P2:
    Update vagrantfile to enable synced folder;
    Change Ingress.yml to reach apps regarding to hostnames;
    Add command to provision (files copy, k3s deplyment);

--- p2/Scripts/server.sh
+++ p2/Scripts/server.sh
@@ -2,3 +2,6 @@
 
 curl -sfL https://get.k3s.io | K3S_TOKEN=pouet sh -s - server --bind-address 192.168.56.110 --flannel-iface=eth1 \
 --write-kubeconfig-mode 644
+
+cp /vagrant/*.yml /home/vagrant/.
+kubectl apply -f .
